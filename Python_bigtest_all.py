90b38e1bb138176c4a2979e3157eea6044fa2b87
RosNarodSport <5@narodsport>
2022-01-29 02:28:57 +0300

    Рабочая версия с показом всего набора одновременно. Задача: - на horizontalSlider_speed посадить увеличение шрифта - размещение текста label по центру

--- Python_bigtest_all.py
+++ Python_bigtest_all.py
@@ -23,14 +23,16 @@ window.show()
 
 def on_click_setBack():
     form.label_main_pushButton.setText("Текст")
-    form.label_for_horizontalSlider1.setText('1')
+    form.label_for_horizontalSlider_size.setText('1')
+    form.horizontalSlider_size.setProperty("value", 1)
+
     form.label_main_pushButton.setFont(QFont('Arial', 12))
+    form.label_hieroglyph.setFont(QFont('Arial', 12))
+
     form.label_for_spinBox.setText('1')
     form.progressBar.setProperty("value", 1)
-    form.horizontalSlider1.setProperty("value", 1)
     form.spinBox.setProperty("value", 1)
     form.dateEdit.setDateTime(QtCore.QDateTime(QDate.currentDate()))
-
 
 
 def on_click():
@@ -41,10 +43,10 @@ def on_click_replace():
     form.label_main_pushButton.setText("replace")
 
 
-def horizontalSlider1_sliderValue():
-    form.progressBar.setValue(form.horizontalSlider1.value())
-    zzz = form.horizontalSlider1.value()
-    form.label_for_horizontalSlider1.setText(str(zzz))
+def horizontalSlider_size_Value():
+    form.progressBar.setValue(form.horizontalSlider_size.value())
+    zzz = form.horizontalSlider_size.value()
+    form.label_for_horizontalSlider_size.setText(str(zzz))
     yyy = form.progressBar.value()
     form.label_for_progressBar.setText(str(yyy))
 
@@ -54,9 +56,9 @@ def horizontalSlider1_sliderValue():
         form.label_hieroglyph.setFont(QFont('Arial', yyy))
 
 
-def spinBox_detail():
+def increase_character_size():
     aaa = form.spinBox.value()
-    form.label_for_spinBox.setText(str(aaa))
+    form.label_hieroglyph.setFont(QFont('Arial', aaa))
 
 
 def dateEdit_use():
@@ -70,14 +72,36 @@ full_list_of_hieroglyphs = [
     [1, '爱', 'ài', 'любить', '妈妈，我爱你。', 'HSK1'],
     [2, '八', 'bā', 'восемь', '他儿子今年八岁了。', 'HSK1'],
     [3, '爸爸', 'bàba', 'отец', '我爸爸是医生。', 'HSK1'],
-    [4, '杯子', 'bēizi', 'стакан, чашка', '杯子里有茶。', 'HSK1']
+    [4, '杯子', 'bēizi', 'стакан, чашка', '杯子里有茶。', 'HSK1'],
+    [5, '北京', 'Běijīng', 'Пекин', '我住在北京。', 'HSK1'],
+    [6, '本', 'běn', 'применяется при счете книг', '桌子上有一本书。', 'HSK1'],
+    [7, '不客气', 'bú kèqi', 'Не стоит.', '甲：谢谢你！乙：不客气。', 'HSK1'],
+    [8, '不', 'bù', 'не, нет', '我不是学生。', 'HSK1'],
+    [9, '菜', 'cài', 'овощи', '我去超市买点儿菜。', 'HSK1'],
+    [10, '茶', 'chá', 'чай', '请喝杯茶吧。', 'HSK1'],
+    [11, '吃', 'chī', 'кушать, есть', '请吃点儿米饭。', 'HSK1'],
+    [12, '出租车', 'chūzūchē', 'такси', '我们坐出租车去火车站。', 'HSK1'],
+    [13, '打电话', 'dǎdiànhuà', 'звонить или говорить по телефону', '他在打电话呢。', 'HSK1'],
+    [14, '大', 'dà', 'большой, крупный', '这个苹果很大。', 'HSK1'],
+    [15, '的', 'de', 'присоединяет определение', '这是我的书。', 'HSK1'],
+    [16, '点', 'diǎn', '(который) час', '现在是下午 3点20。', 'HSK1'],
+    [17, '电脑', 'diànnǎo', 'компьютер', '我买了个电脑。', 'HSK1'],
+    [18, '电视', 'diànshì', 'телевизор', '妈妈在看电视。', 'HSK1'],
+    [19, '电影', 'diànyǐng', 'кинофильм', '我喜欢看电影。', 'HSK1'],
+    [20, '东西', 'dōngxi', 'вещь, предмет', '我在商店买了很多东西。', 'HSK1']
 ]
+
+# Метод управляет изменением скорости показа иероглифа
+def increase_speed_show():
+    speed = 1000*1
+    return speed
 
 
 def show_me_hieroglyphs():
     for i in range(0, len(full_list_of_hieroglyphs)):
         one_dictionary_entry = full_list_of_hieroglyphs[i]
-        QtCore.QTimer.singleShot(2000 * i, partial(update, one_dictionary_entry, i))
+        speed = increase_speed_show()
+        QtCore.QTimer.singleShot(speed * i, partial(update, one_dictionary_entry, i))
 
 
 def update(one_dictionary_entry, i):
@@ -92,7 +116,7 @@ def update(one_dictionary_entry, i):
         form.label_HSK.setText(str(one_dictionary_entry[5]))
         y += 1
 
-print('Что тут?')
+
 # show_me_hieroglyphs()
 
 # update(full_list_of_hieroglyphs, 0)
@@ -104,6 +128,7 @@ def vertical_print_one_complect_of_hieroglyph(complect_of_hieroglyph):
             y = str(y)
             # print(delimiter.join(y))
 
+
 def hieroglyphs():
     form.label_in_groupBox1.setText('我是')
 
@@ -111,11 +136,17 @@ def hieroglyphs():
 vertical_print_one_complect_of_hieroglyph(full_list_of_hieroglyphs)
 form.main_pushButton.clicked.connect(on_click)
 form.pushButton_setBack.clicked.connect(on_click_setBack)
-form.horizontalSlider1.valueChanged.connect(horizontalSlider1_sliderValue)
-form.spinBox.valueChanged.connect(spinBox_detail)
+
+form.spinBox.valueChanged.connect(increase_character_size)
 form.dateEdit.dateTimeChanged.connect(dateEdit_use)
 
-# form.pushButton_replace.clicked.connect(hieroglyphs)
+# Запуск показа статей
 form.pushButton_replace.clicked.connect(show_me_hieroglyphs)
+
+# Слайдер управления размером иероглифа при показе
+form.horizontalSlider_size.valueChanged.connect(horizontalSlider_size_Value)
+
+# Слайдер управления скоростью показа статьи
+form.horizontalSlider_speed.valueChanged.connect(increase_speed_show)
 
 app.exec_()
